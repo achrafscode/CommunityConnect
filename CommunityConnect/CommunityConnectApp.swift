f92fb28db3f1ce253a9bc8b81cee6c8458755411
Achraf Zemzami <assilabouchraz6@gmail.com>
2023-10-26 11:30:21 -0400

    Initial Commit

---
+++ CommunityConnect/CommunityConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityConnectApp.swift
+//  CommunityConnect
+//
+//  Created by Achraf Zemzami on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CommunityConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
