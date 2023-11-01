ebc921d55648c7f08a35bc647ace6a3ee43c09b8
Achraf Zemzami <assilabouchraz6@gmail.com>
2023-11-01 11:25:53 -0400

    HomeScreen started by Justin.

--- CommunityConnect/Subsections/HomeScreen.swift
+++ CommunityConnect/Subsections/HomeScreen.swift
@@ -7,18 +7,41 @@
 import SwiftUI
 import Foundation
 
+class EventStore: ObservableObject {
+    @Published var events: [Event] = []
+
+    func addEvent(_ event: Event) {
+        events.append(event)
+    }
+}
+
 struct HomeScreen: View {
+    @StateObject var eventStore = EventStore()
+    @State private var isAddingEvent = false
+
     var body: some View {
-        VStack {
-            Spacer()
-            HStack {
-                //HomeButton()
-                //ResourcesButton()
-                //ManageButton()
+        NavigationView {
+            VStack {
+                Text("Newark Events")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                    .padding()
+
+                Spacer()
+
+                Button("Create Event") {
+                    isAddingEvent.toggle()
+                }
+                .padding()
+                
+                List(eventStore.events, id: \.name) { event in
+                    EventRow(event: event)
+                }
             }
-            Text("Hello") // Add this Text view
-                .font(.title)
-            Spacer()
+        }
+        .background(Color.green)
+        .sheet(isPresented: $isAddingEvent) {
+            CreateEventView(eventStore: eventStore)
         }
     }
 }
@@ -34,5 +57,58 @@ struct HomeButton: View {
 
         }
         .padding()
+    }
+}
+
+/*struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
+*/
+struct CreateEventView: View {
+    @ObservedObject var eventStore: EventStore
+
+    @State private var name = ""
+    @State private var description = ""
+    @State private var location = ""
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Event Information")) {
+                    TextField("Name", text: $name)
+                    TextField("Description", text: $description)
+                    TextField("Location", text: $location)
+                }
+            }
+            .navigationBarTitle("Create Event")
+            .navigationBarItems(trailing: Button("Create") {
+                let event = Event(name: name, description: description, location: location)
+                eventStore.addEvent(event)
+            })
+        }
+    }
+}
+
+struct Event: Identifiable {
+    let id = UUID()
+    let name: String
+    let description: String
+    let location: String
+}
+
+struct EventRow: View {
+    let event: Event
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(event.name)
+                .font(.headline)
+            Text(event.description)
+                .font(.body)
+            Text(event.location)
+                .font(.body)
+        }
     }
 }
